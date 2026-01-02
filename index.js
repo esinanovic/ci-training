e2eafafed300add77420d5ba017a065a339a3a30
Emir <emirsinanov@gmail.com>
2026-01-03 00:45:18 +0100

    V1 FINAL

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { addition, multiplication } = require('./math');
 console.log(`Addition: 3 + 2 = ${addition(3, 2)}`);
 console.log(`Multiplication: 4 * 5 = ${multiplication(4, 5)}`);
 
+
+
 module.exports = {
   addition,
   multiplication
